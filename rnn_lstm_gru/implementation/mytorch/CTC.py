d84dc01e9cd6e449f8c2fb699c5e41bae0ba38c1
Krish Rana <krishrana@Krishs-MacBook-Air-3.local>
2023-05-28 18:55:59 -0700

    RNNs

---
+++ rnn_lstm_gru/implementation/mytorch/CTC.py
@@ -0,0 +1,305 @@
+import numpy as np
+
+import loss
+
+
+class CTC(object):
+
+    def __init__(self, BLANK=0):
+        """
+		
+		Initialize instance variables
+
+		Argument(s)
+		-----------
+		
+		BLANK (int, optional): blank label index. Default 0.
+
+		"""
+
+        # No need to modify
+        self.BLANK = BLANK
+
+    def extend_target_with_blank(self, target):
+        """Extend target sequence with blank.
+
+        Input
+        -----
+        target: (np.array, dim = (target_len,))
+                target output
+        ex: [B,IY,IY,F]
+
+        Return
+        ------
+        extSymbols: (np.array, dim = (2 * target_len + 1,))
+                    extended target sequence with blanks
+        ex: [-,B,-,IY,-,IY,-,F,-]
+
+        skipConnect: (np.array, dim = (2 * target_len + 1,))
+                    skip connections
+        ex: [0,0,0,1,0,0,0,1,0]
+		"""
+
+        extended_symbols = [self.BLANK]
+        for symbol in target:
+            extended_symbols.append(symbol)
+            extended_symbols.append(self.BLANK)
+
+        N = len(extended_symbols)
+        target_len = len(target)
+
+        skip_connect = np.zeros_like(extended_symbols)
+        extended_symbols = np.array(extended_symbols).reshape((N,))
+        skip_connect = np.array(skip_connect).reshape((N,))
+
+        i = 2
+        while i < N:
+            j = i - 2
+            if (extended_symbols[i] != self.BLANK) and (extended_symbols[i] != extended_symbols[j]):
+                skip_connect[i] = 1
+            i = i + 1
+
+        return extended_symbols, skip_connect
+
+    def get_forward_probs(self, logits, extended_symbols, skip_connect):
+        """Compute forward probabilities.
+
+        Input
+        -----
+        logits: (np.array, dim = (input_len, len(Symbols)))
+                predict (log) probabilities
+
+                To get a certain symbol i's logit as a certain time stamp t:
+                p(t,s(i)) = logits[t, qextSymbols[i]]
+
+        extSymbols: (np.array, dim = (2 * target_len + 1,))
+                    extended label sequence with blanks
+
+        skipConnect: (np.array, dim = (2 * target_len + 1,))
+                    skip connections
+
+        Return
+        ------
+        alpha: (np.array, dim = (input_len, 2 * target_len + 1))
+                forward probabilities
+
+        """
+        input_len, N = len(logits), len(extended_symbols)
+        alpha = np.zeros(shape=(input_len, N))
+
+        alpha[0][0] = logits[0][extended_symbols[0]]
+        alpha[0][1] = logits[0][extended_symbols[1]]
+
+        for t in range(1, input_len):
+            alpha[t][0] = alpha[t - 1][0] * logits[t][extended_symbols[0]]
+            for l in range(1, N):
+                alpha[t][l] = alpha[t - 1][l] + alpha[t - 1][l - 1]
+                if skip_connect[l]:
+                    alpha[t][l] += alpha[t - 1][l - 2]
+                alpha[t][l] *= logits[t][extended_symbols[l]]
+
+        return alpha
+
+    def get_backward_probs(self, logits, extended_symbols, skip_connect):
+        """Compute backward probabilities.
+
+        Input
+        -----
+        logits: (np.array, dim = (input_len, len(symbols)))
+                predict (log) probabilities
+
+                To get a certain symbol i's logit as a certain time stamp t:
+                p(t,s(i)) = logits[t,extSymbols[i]]
+
+        extSymbols: (np.array, dim = (2 * target_len + 1,))
+                    extended label sequence with blanks
+
+        skipConnect: (np.array, dim = (2 * target_len + 1,))
+                    skip connections
+
+        Return
+        ------
+        beta: (np.array, dim = (input_len, 2 * target_len + 1))
+                backward probabilities
+		
+		"""
+
+        T, N = len(logits), len(extended_symbols)
+        beta = np.zeros(shape=(T, N))
+        beta[T - 1][N - 1] = logits[T - 1][extended_symbols[N - 1]]
+        beta[T - 1][N - 2] = logits[T - 1][extended_symbols[N - 2]]
+
+        for t in range(T - 2, -1, -1):
+            beta[t][N - 1] = beta[t + 1][N - 1] * logits[t][extended_symbols[N - 1]]
+            for i in range(N - 2, -1, -1):
+                beta[t][i] = beta[t + 1][i] + beta[t + 1][i + 1]
+                if (i <= N - 3) and (extended_symbols[i] != extended_symbols[i + 2]):
+                    beta[t][i] += beta[t + 1][i + 2]
+                beta[t][i] *= logits[t][extended_symbols[i]]
+
+        for t in range(T - 1, -1, -1):
+            for i in range(N - 1, -1, -1):
+                beta[t][i] = beta[t][i] / logits[t][extended_symbols[i]]
+
+        return beta
+
+    def get_posterior_probs(self, alpha, beta):
+        """Compute posterior probabilities.
+
+        Input
+        -----
+        alpha: (np.array, dim = (input_len, 2 * target_len + 1))
+                forward probability
+
+        beta: (np.array, dim = (input_len, 2 * target_len + 1))
+                backward probability
+
+        Return
+        ------
+        gamma: (np.array, dim = (input_len, 2 * target_len + 1))
+                posterior probability
+
+		"""
+
+        [T, N] = alpha.shape
+        gamma = np.zeros(shape=(T, N))
+        sumgamma = np.zeros((T,))
+
+        for t in range(T):
+            for i in range(N):
+                gamma[t][i] = alpha[t][i] * beta[t][i]
+                sumgamma[t] += gamma[t][i]
+
+            for i in range(N):
+                gamma[t][i] = gamma[t][i] / sumgamma[t]
+
+        return gamma
+
+
+class CTCLoss(object):
+
+    def __init__(self, BLANK=0):
+        """
+		Initialize instance variables
+
+        Argument(s)
+		-----------
+		BLANK (int, optional): blank label index. Default 0.
+        
+		"""
+
+        # No need to modify
+        super(CTCLoss, self).__init__()
+
+        self.BLANK = BLANK
+        self.gammas = []
+        self.extended_symbols = []
+        self.ctc = CTC()
+
+    def __call__(self, logits, target, input_lengths, target_lengths):
+
+        # No need to modify
+        return self.forward(logits, target, input_lengths, target_lengths)
+
+    def forward(self, logits, target, input_lengths, target_lengths):
+        """CTC loss forward
+
+		Computes the CTC Loss by calculating forward, backward, and
+		posterior proabilites, and then calculating the avg. loss between
+		targets and predicted log probabilities
+
+        Input
+        -----
+        logits [np.array, dim=(seq_length, batch_size, len(symbols)]:
+			log probabilities (output sequence) from the RNN/GRU
+
+			            y_hat = logits[:i_len, batch_itr, :]
+
+
+        target [np.array, dim=(batch_size, padded_target_len)]:
+            target sequences
+
+        input_lengths [np.array, dim=(batch_size,)]:
+            lengths of the inputs
+
+        target_lengths [np.array, dim=(batch_size,)]:
+            lengths of the target
+
+        Returns
+        -------
+        loss [float]:
+            avg. divergence between the posterior probability and the target
+
+        """
+
+        # No need to modify
+        self.logits = logits
+        self.target = target
+        self.input_lengths = input_lengths
+        self.target_lengths = target_lengths
+
+        #####  IMP:
+        #####  Output losses will be divided by the target lengths
+        #####  and then the mean over the batch is taken
+
+        # No need to modify
+        B, _ = target.shape
+        total_loss = np.zeros(B)
+        # self.extended_symbols = []
+
+        for batch_itr in range(B):
+
+            ctc = CTC()
+            cross_entropy = loss.SoftmaxCrossEntropy()
+
+            # Truncate the target to target length
+            y_len = target_lengths[batch_itr]
+            y = target[batch_itr, :y_len]
+
+            # Truncate the logits to input length
+            i_len = input_lengths[batch_itr]
+            y_hat = logits[:i_len, batch_itr, :]
+
+            # Extend target sequence with blank
+            y_extend, skip_connect = ctc.extend_target_with_blank(y)
+
+            # Compute forward probabilities
+            alpha = ctc.get_forward_probs(y_hat, y_extend, skip_connect)
+
+            # Compute backward probabilities
+            beta = ctc.get_backward_probs(y_hat, y_extend, skip_connect)
+
+            # Compute posteriors using total probability function
+            prob_posterior = ctc.get_posterior_probs(alpha, beta)
+            self.gammas.append(prob_posterior)
+            self.extended_symbols.append(y_extend)
+
+            T, r = y_hat.shape[0], len(y_extend)
+
+            for t in range(T):
+                for sys_r in range(r):
+                    total_loss[batch_itr] += (prob_posterior[t, sys_r] * np.log(y_hat[t, y_extend[sys_r]]))
+
+        total_loss = -np.sum(total_loss) / B
+
+        return total_loss
+
+    def backward(self):
+
+        # No need to modify
+        T, B, C = self.logits.shape
+        dY = np.full_like(self.logits, 0)
+
+        for batch_itr in range(B):
+            y = self.extended_symbols[batch_itr]
+            N = len(y)
+            i_len = self.input_lengths[batch_itr]
+            y_hat = self.logits[:i_len, batch_itr, :]
+            prob_posterior = self.gammas[batch_itr]
+            T = y_hat.shape[0]
+
+            for t in range(T):
+                for i in range(N):
+                    dY[t, batch_itr, y[i]] -= (prob_posterior[t, i] / y_hat[t, y[i]])
+
+        return dY
---
+++ rnn_lstm_gru/implementation/mytorch/gru_cell.py
@@ -0,0 +1,209 @@
+import numpy as np
+from activation import *
+
+
+class GRUCell(object):
+    """GRU Cell class."""
+
+    def __init__(self, in_dim, hidden_dim):
+        self.d = in_dim
+        self.h = hidden_dim
+        h = self.h
+        d = self.d
+        self.x_t = 0
+
+        self.Wrx = np.random.randn(h, d)
+        self.Wzx = np.random.randn(h, d)
+        self.Wnx = np.random.randn(h, d)
+
+        self.Wrh = np.random.randn(h, h)
+        self.Wzh = np.random.randn(h, h)
+        self.Wnh = np.random.randn(h, h)
+
+        self.brx = np.random.randn(h)
+        self.bzx = np.random.randn(h)
+        self.bnx = np.random.randn(h)
+
+        self.brh = np.random.randn(h)
+        self.bzh = np.random.randn(h)
+        self.bnh = np.random.randn(h)
+
+        self.dWrx = np.zeros((h, d))
+        self.dWzx = np.zeros((h, d))
+        self.dWnx = np.zeros((h, d))
+
+        self.dWrh = np.zeros((h, h))
+        self.dWzh = np.zeros((h, h))
+        self.dWnh = np.zeros((h, h))
+
+        self.dbrx = np.zeros((h))
+        self.dbzx = np.zeros((h))
+        self.dbnx = np.zeros((h))
+
+        self.dbrh = np.zeros((h))
+        self.dbzh = np.zeros((h))
+        self.dbnh = np.zeros((h))
+
+        self.r_act = Sigmoid()
+        self.z_act = Sigmoid()
+        self.h_act = Tanh()
+
+        # Define other variables to store forward results for backward here
+
+    def init_weights(self, Wrx, Wzx, Wnx, Wrh, Wzh, Wnh, brx, bzx, bnx, brh, bzh, bnh):
+        self.Wrx = Wrx
+        self.Wzx = Wzx
+        self.Wnx = Wnx
+        self.Wrh = Wrh
+        self.Wzh = Wzh
+        self.Wnh = Wnh
+        self.brx = brx
+        self.bzx = bzx
+        self.bnx = bnx
+        self.brh = brh
+        self.bzh = bzh
+        self.bnh = bnh
+
+    def __call__(self, x, h_prev_t):
+        return self.forward(x, h_prev_t)
+
+    def forward(self, x, h_prev_t):
+        """GRU cell forward.
+
+        Input
+        -----
+        x: (input_dim)
+            observation at current time-step.
+
+        h_prev_t: (hidden_dim)
+            hidden-state at previous time-step.
+
+        Returns
+        -------
+        h_t: (hidden_dim)
+            hidden state at current time-step.
+
+        """
+        self.x = x
+        self.hidden = h_prev_t
+
+        self.r_x = self.Wrx @ self.x + self.brx
+        self.r_h = self.Wrh @ h_prev_t + self.brh
+        self.r = self.r_act(self.r_x + self.r_h)
+
+        self.z_x = self.Wzx @ self.x + self.bzx
+        self.z_h = self.Wzh @ h_prev_t + self.bzh
+        self.z = self.z_act(self.z_x + self.z_h)
+
+        self.n_x = self.Wnx @ self.x + self.bnx
+        self.n_h = self.Wnh @ h_prev_t + self.bnh
+        self.n_pre_act = self.n_x + self.r * (self.n_h)
+        self.n = self.h_act(self.n_pre_act)
+
+        h_t = (1 - self.z) * self.n + self.z * h_prev_t
+
+        assert self.x.shape == (self.d,)
+        assert self.hidden.shape == (self.h,)
+
+        assert self.r.shape == (self.h,)
+        assert self.z.shape == (self.h,)
+        assert self.n.shape == (self.h,)
+        assert h_t.shape == (self.h,)  # h_t is the final output of you GRU cell.
+
+        return h_t
+
+    def backward(self, delta):
+        """GRU cell backward.
+
+        This must calculate the gradients wrt the parameters and return the
+        derivative wrt the inputs, xt and ht, to the cell.
+
+        Input
+        -----
+        delta: (hidden_dim)
+                summation of derivative wrt loss from next layer at
+                the same time-step and derivative wrt loss from same layer at
+                next time-step.
+
+        Returns
+        -------
+        dx: (1, input_dim)
+            derivative of the loss wrt the input x.
+
+        dh_prev_t: (1, hidden_dim)
+            derivative of the loss wrt the input hidden h.
+
+        """
+
+        self.x = self.x.reshape(1, -1)
+        h = self.hidden.reshape(1, -1)
+        z = self.z.reshape(1, -1)
+        n = self.n.reshape(1, -1)
+        r = self.r.reshape(1, -1)
+        n_h = self.n_h.reshape(1, -1)
+
+        tanh_backward = self.h_act.backward(n).reshape(1, -1)
+        z_backward = self.z_act.backward().reshape(1, -1)
+        r_backward = self.r_act.backward().reshape(1, -1)
+
+        dn = delta * (1 - z)
+        dz = delta * (h - n)
+        dr = dn * tanh_backward * (n_h)
+
+        # dx
+        par_dn = dn @ (tanh_backward.T * self.Wnx)
+        par_dz = dz @ (z_backward.T * self.Wzx)
+        par_dr = dr @ (r_backward.T * self.Wrx)
+
+        dx = par_dn + par_dz + par_dr
+
+        # dh_prev_t
+        par_dh = delta * z
+        par_dnh = (dn * tanh_backward * r) @ self.Wnh
+        par_dzh = (dz * z_backward) @ self.Wzh
+        par_drh = (dr * r_backward) @ self.Wrh
+
+        dh_prev_t = par_dh + par_dnh + par_dzh + par_drh
+
+        # dWrx
+        par_r = (dr * r_backward).T
+        self.dWrx = par_r @ self.x
+
+        # dWzx
+        par_z = (dz * z_backward).T
+        self.dWzx = par_z @ self.x
+
+        # d_wnx
+        self.dWnx = (dn * tanh_backward).T @ self.x
+
+        # dWrh
+        self.dWrh = par_r @ h
+
+        # dWzh
+        self.dWzh = par_z @ h
+
+        # d_wnh
+        self.dWnh = (dn * tanh_backward * r).T @ h
+
+        # dbrx
+        self.dbrx = par_r.T
+
+        # dbzx
+        self.dbzx = par_z.T
+
+        # d_bnx
+        self.dbnx = dn * tanh_backward
+
+        # dbrh
+        self.dbrh = par_r.T
+
+        # dbzh
+        self.dbzh = par_z.T
+
+        # d_bnh
+        self.dbnh = dn * tanh_backward * r
+
+        assert dx.shape == (1, self.d)
+        assert dh_prev_t.shape == (1, self.h)
+
+        return dx, dh_prev_t
